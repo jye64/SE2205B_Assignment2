61d8c24ac798d1b9ce81c8186452d32ebc62a39b
jye64 <jye64@uwo.ca>
2018-03-25 19:37:45 -0400

    DataKey class

--- Birds/src/birds/DataKey.java
+++ Birds/src/birds/DataKey.java
@@ -1,21 +1,49 @@
 package birds;
 
 public class DataKey {
-
     
-	// default constructor
-	public DataKey() {
+    private String birdName;
+    private int birdSize;
+    
+    
+    // default constructor
+    public DataKey() {
 	 
-	}
-        
-	// other constructors
+    } 
+    
+    public String getBirdName(){
+        return birdName;
+    }
+    
+    public int getBirdSize(){
+        return birdSize;
+    }
+       
+    // other constructors
+    
+    public DataKey(String name, int size){
+        birdName = name;
+        birdSize = size;
+    }
 
-	/**
-	 * Returns 0 if this DataKey is equal to k, returns -1 if this DataKey is smaller
-	 * than k, and it returns 1 otherwise. 
-	 */
-	public int compareTo(DataKey k) {
-           return 0;
-            
-	}
+    /**
+    * Returns 0 if this DataKey is equal to k, returns -1 if this DataKey is smaller
+    * than k, and it returns 1 otherwise. 
+    */
+    public int compareTo(DataKey k) {
+        
+        if (this.birdSize > k.birdSize){
+            return 1;
+        } else if (this.birdSize < k.birdSize){
+            return -1;
+        } else {               //determine order alphabetically
+            if (this.birdName.compareTo(k.birdName)>0){
+                return 1;  
+            } else if (this.birdName.compareTo(k.birdName)==0)
+                return 0;
+            else
+                return -1;
+        }
+                
+    }
 }
