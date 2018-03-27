1ba47775bcb4323e412464c0c48b0c03e1483314
jye64 <jye64@uwo.ca>
2018-03-27 16:32:36 -0400

    OrderedDictionary

---
+++ Birds/src/birds/OrderedDictionary.java
@@ -0,0 +1,225 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package birds;
+
+import java.util.Stack;
+
+/**
+ *
+ * @author alanye
+ */
+public class OrderedDictionary implements OrderedDictionaryADT{
+    class Node{
+        BirdRecord birdRecord;
+        Node left;
+        Node right;
+        Node parent;
+        
+        public Node(BirdRecord record){
+            this.birdRecord = record;
+            left = null;
+            right = null; 
+            parent = null;
+        }
+    } // end class Node
+    
+    private Node root;
+    
+    @Override
+    public BirdRecord find(DataKey k) throws DictionaryException {
+        Node current = root;
+        while(current!=null){
+            if(current.birdRecord.getDataKey().compareTo(k)==0){
+                return current.birdRecord;
+            }else if (current.birdRecord.getDataKey().compareTo(k)==1){
+                current = current.left;
+            }else{
+                current = current.right;
+            }
+        }
+        throw new DictionaryException("There is no record matches the given key"); 
+    }
+
+    @Override
+    public void insert(BirdRecord r) throws DictionaryException {
+        Node newNode = new Node(r);
+        if (root==null){
+            root = newNode;
+            return;
+        }
+        Node current = root;
+        Node parent = null;
+        while(true){
+            parent = current;
+            if (r.getDataKey().compareTo(current.birdRecord.getDataKey())==-1){
+                current = current.left;
+                if (current==null){
+                    parent.left = newNode;
+                    newNode.parent = parent;
+                    return;
+                }
+            }else if(r.getDataKey().compareTo(current.birdRecord.getDataKey())==0){
+                throw new DictionaryException("A BirdRecord with the same key is found.");  
+            }else{
+                current = current.right;
+                if (current == null){
+                    parent.right = newNode;
+                    newNode.parent = parent;
+                    return;
+                }     
+            }
+        }  
+    }
+   
+    
+    @Override
+    public void remove(DataKey k) throws DictionaryException {
+        //NB: throw exception in deleteRec
+        deleteRec(root,k);  
+    }
+    
+    private Node deleteRec(Node root, DataKey key) throws DictionaryException
+    {
+        /* Base Case: If the tree is empty */
+        if (root == null)  return root;
+ 
+        /* Otherwise, recur down the tree */
+        if (key.compareTo(root.birdRecord.getDataKey())==-1)
+            root.left = deleteRec(root.left, key);
+        else if (key.compareTo(root.birdRecord.getDataKey())==1)
+            root.right = deleteRec(root.right, key);
+ 
+        // if key is same as root's key, then This is the node
+        // to be deleted
+        else
+        {
+            // node with only one child or no child
+            if (root.left == null)
+                return root.right;
+            else if (root.right == null)
+                return root.left;
+ 
+            // node with two children: Get the inorder successor (smallest
+            // in the right subtree)
+            root = findMin(root.right);
+ 
+            // Delete the inorder successor
+            root.right = deleteRec(root.right, root.birdRecord.getDataKey());
+        }
+        
+        if(root==null)
+            throw new DictionaryException("No such record key exists");
+ 
+        return root;
+    }
+
+    
+
+    @Override
+    public BirdRecord successor(DataKey k) throws DictionaryException {
+        
+        Node node = root, successor = null;
+        while (node != null) {
+            if (node.birdRecord.getDataKey().compareTo(k)==1) {
+                successor = node;
+                node = node.left;
+            } else {
+                node = node.right;
+            }
+        }
+        return successor.birdRecord;
+      
+    }
+    
+
+    @Override
+    public BirdRecord predecessor(DataKey k) throws DictionaryException {
+        
+        Node node = root, predecessor = null;
+        while(node!=null){
+            if(node.birdRecord.getDataKey().compareTo(k)==-1){
+                predecessor = node;
+                node = node.right;
+            }else{
+                node = node.left;
+            }
+        }
+        return predecessor.birdRecord;
+   
+    }
+    
+
+    @Override
+    public BirdRecord smallest() throws DictionaryException {
+        Node current = root;
+        if (root==null){
+            throw new DictionaryException("Dictionary is empty");
+        }
+        while (current.left!= null){
+            current = current.left;
+        }
+        return current.birdRecord;
+    }
+
+    @Override
+    public BirdRecord largest() throws DictionaryException {
+        Node current = root;
+        if (root==null){
+            throw new DictionaryException("Dictionary is empty");
+        }
+        while(current.right != null){
+            current = current.right;
+        }
+        return current.birdRecord;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return root == null;
+    }
+ 
+//>>>>>>>>>>> ADDED PRIVATE METHODS >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+   
+    //find Node with DataKey k
+    private Node findNodeWithKey(DataKey k) throws DictionaryException{
+        Node current = root;
+        while (root!=null){
+            if(current.birdRecord.getDataKey().compareTo(k)==0){
+                return current;
+            }else if (current.birdRecord.getDataKey().compareTo(k)==1){
+                current = current.left;
+            }else {
+                current = current.right;
+            }
+        }
+        throw new DictionaryException("No Node with such key.");
+    }
+    
+    //find the minimum in right subtree
+    private Node findMin(Node root){
+        if(root==null){
+            return root;
+        }
+        while(root.left!=null){
+            root = root.left;
+        }
+        return root;
+    }
+    
+    //find the maximum in left subtree
+    private Node findMax(Node root){
+        if(root==null){
+            return null;
+        }
+        while(root.right!=null){
+            root = root.right;
+        }
+        return root;
+    }
+}
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+        
--- Birds/src/birds/OrderedDictionaryADT.java
+++ Birds/src/birds/OrderedDictionaryADT.java
@@ -3,8 +3,9 @@ package birds;
 public interface OrderedDictionaryADT {
 
     /* Ordered Dictionary ADT */
-
- /* Returns the Record object with key k, or It throws a DictionaryException
+   
+    public BirdRecord find(DataKey k) throws DictionaryException;
+     /* Returns the Record object with key k, or It throws a DictionaryException
        says: "There is no record matches the given key", if such a record
        is not in the dictionary. 
 
@@ -12,16 +13,16 @@ public interface OrderedDictionaryADT {
        @return BirdRecord
        @throws DictionaryException
      */
-    public BirdRecord find(DataKey k) throws DictionaryException;
 
+    public void insert(BirdRecord r) throws DictionaryException;
     /* Inserts r into the ordered dictionary. It throws a DictionaryException 
        if a record with the same key as r is already in the dictionary.  
 
        @param r
        @throws DictionaryException
      */
-    public void insert(BirdRecord r) throws DictionaryException;
 
+    public void remove(DataKey k) throws DictionaryException;
     /*  Removes the record with Key k from the dictionary. It throws a 
         DictionaryException says: "No such record key exists", if the record
         is not in the dictionary. 
@@ -29,8 +30,9 @@ public interface OrderedDictionaryADT {
        @param k
        @throws DictionaryException
      */
-    public void remove(DataKey k) throws DictionaryException;
-
+    
+    public BirdRecord successor(DataKey k) throws DictionaryException;
+    
     /* Returns the successor of k (the record from the ordered dictionary 
        with smallest key larger than k); It throws a DictionaryException says:
        "There is no successor for the given record key", if the given key has 
@@ -40,8 +42,10 @@ public interface OrderedDictionaryADT {
        @return BirdRecord
        @throws DictionaryException
      */
-    public BirdRecord successor(DataKey k) throws DictionaryException;
 
+
+    public BirdRecord predecessor(DataKey k) throws DictionaryException;
+    
     /* Returns the predecessor of k (the record from the ordered dictionary 
        with largest key smaller than k; It throws a DictionaryException says:
        "There is no predecessor for the given record key", if the given key has 
@@ -51,8 +55,9 @@ public interface OrderedDictionaryADT {
        @return BirdRecord
        @throws DictionaryException
      */
-    public BirdRecord predecessor(DataKey k) throws DictionaryException;
 
+
+    public BirdRecord smallest() throws DictionaryException;
     /* Returns the record with smallest key in the ordered dictionary. 
        It throws a DictionaryException says:"Dictionary is empty", if the 
        dictionary is empty.   
@@ -60,19 +65,20 @@ public interface OrderedDictionaryADT {
        @return BirdRecord
        @throws DictionaryException
      */
-    public BirdRecord smallest() throws DictionaryException;
 
+
+    public BirdRecord largest() throws DictionaryException;
     /* Returns the record with largest key in the ordered dictionary. 
        It throws a DictionaryException says:"Dictionary is empty", if the 
        dictionary is empty.  
        @return BirdRecord
        @throws DictionaryException
      */
-    public BirdRecord largest() throws DictionaryException;
 
+
+    public boolean isEmpty();
     /* Returns true if the dictionary is empty, and true otherwise. 
 
        @return boolean
      */
-    public boolean isEmpty();
 }
