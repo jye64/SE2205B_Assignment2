b334c05269ff264cf5d80555610f78468188bf53
jye64 <jye64@uwo.ca>
2018-03-29 23:48:42 -0400

    BirdsController

--- Birds/src/birds/BirdsController.java
+++ Birds/src/birds/BirdsController.java
@@ -5,11 +5,27 @@
  */
 package birds;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.Scanner;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.MenuBar;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 /**
@@ -21,18 +37,230 @@ public class BirdsController implements Initializable {
     @FXML
     private MenuBar mainMenu;
     
-
     @FXML
-    public void exit() {
-        Stage stage = (Stage) mainMenu.getScene().getWindow();
-        stage.close();
+    private Pane birdImageView;
+    
+    @FXML
+    private ComboBox birdSize;
+    
+    @FXML
+    private TextField birdName;
+          
+    @FXML
+    private Label birdNameDisplayed;
+    
+    @FXML
+    private Label birdText;
+   
+    @FXML
+    Button find;
+    @FXML
+    Button first;
+    @FXML
+    Button next;
+    @FXML
+    Button previous;
+    @FXML
+    Button last;
+    @FXML
+    Button delete;
+    @FXML
+    Button play;
+    @FXML
+    Button stop;
+     
+    private final OrderedDictionary birdDictionary = new OrderedDictionary();
+    private BirdRecord birdRecord = new BirdRecord();   
+    private Media birdSound;
+    private MediaPlayer player;
+    String dictionaryFileName = "BirdsDataBase.txt";  
+    
+    @FXML
+    public void loadDictionary(){
+        Scanner input;
+        try{
+            int bird_Size;
+            String bird_Name;
+            String bird_About;
+            String bird_Image;
+            String bird_Sound;
+            //boolean isLoaded =false;
+            
+            input = new Scanner(new File(dictionaryFileName));
+            while(input.hasNextLine()){
+                for (int i=0; i<3; i++){
+                    String temp = input.nextLine();
+                    if(temp.length()<=0)
+                        continue;
+                    bird_Size = Integer.parseInt(temp);
+                    bird_Name = input.nextLine();
+                    bird_About = input.nextLine();
+                    bird_Image = "src/images/"+bird_Name+".jpg";
+                    bird_Sound = "src/sounds/"+bird_Name+".mp3";
+                    DataKey newKey = new DataKey(bird_Name, bird_Size);
+                    BirdRecord newRecord = new BirdRecord(newKey,bird_About,bird_Sound,bird_Image);
+                    try{
+                        birdDictionary.insert(newRecord);
+                    }catch(DictionaryException e){
+                        displayAlert("ERROR: "+e.getMessage());
+                    }
+//                    if(!input.hasNextLine()){
+//                        isLoaded = true;
+//                        System.out.println("Dictionary is loaded");
+//                    }
+                }
+            }
+ 
+        }catch(IOException ex){
+            displayAlert("ERROR: "+ex.getMessage());
+        }
+         
+    }
+    
+    @FXML
+    public void first(){
+        try{
+            birdRecord = birdDictionary.smallest();
+            birdNameDisplayed.setText(birdRecord.getDataKey().getBirdName());
+            birdText.setText(birdRecord.getAbout());
+            birdImageView.getChildren().add(new ImageView(new Image(new File(birdRecord.getImage()).toURI().toString())));
+            System.out.println(new File(birdRecord.getImage()).toURI().toString());
+        }catch (DictionaryException ex){
+            displayAlert("ERROR: "+ ex.getMessage());
+        }   
+    }
+    
+    @FXML
+    public void last(){
+        try{
+            birdRecord = birdDictionary.largest();
+            birdNameDisplayed.setText(birdRecord.getDataKey().getBirdName());
+            birdText.setText(birdRecord.getAbout());
+            birdImageView.getChildren().add(new ImageView(new Image(new File(birdRecord.getImage()).toURI().toString())));        
+        }catch(DictionaryException ex){
+            displayAlert("ERROR: "+ ex.getMessage());
+        }
+    }
+    
+    @FXML
+    public void next() throws DictionaryException{
+        try{
+            birdRecord = birdDictionary.successor(birdRecord.getDataKey());
+            birdNameDisplayed.setText(birdRecord.getDataKey().getBirdName());
+            birdText.setText(birdRecord.getAbout());
+            birdImageView.getChildren().add(new ImageView(new Image(new File(birdRecord.getImage()).toURI().toString())));
+        }catch (DictionaryException ex){
+            displayAlert("ERROR: "+ ex.getMessage());
+        }
+    }
+    
+    @FXML
+    public void previous() throws DictionaryException{
+        try{
+            birdRecord = birdDictionary.predecessor(birdRecord.getDataKey());
+            birdNameDisplayed.setText(birdRecord.getDataKey().getBirdName());
+            birdText.setText(birdRecord.getAbout());
+            birdImageView.getChildren().add(new ImageView(new Image(new File(birdRecord.getImage()).toURI().toString())));
+        }catch(DictionaryException ex){
+            displayAlert("ERROR: "+ ex.getMessage());
+        }
     }
 
+    @FXML
+    public void delete() throws DictionaryException{
+        try{
+            birdDictionary.remove(birdRecord.getDataKey());
+            if(birdDictionary.isEmpty()){
+                birdNameDisplayed.setText("");
+                birdText.setText("");
+                birdImageView.getChildren().clear();
+            }else{
+                birdRecord = birdDictionary.successor(birdRecord.getDataKey());
+                birdNameDisplayed.setText(birdRecord.getDataKey().getBirdName());
+                birdText.setText(birdRecord.getAbout());
+                birdImageView.getChildren().add(new ImageView(new Image(new File(birdRecord.getImage()).toURI().toString())));  
+            }
+        }catch(DictionaryException ex){
+            displayAlert("ERROR: "+ ex.getMessage());
+        }
+    }
     
-
+    @FXML
+    public void find(){
+        DataKey newKey = new DataKey(birdName.getText(), readCombo());
+        try{
+            birdRecord = birdDictionary.find(newKey);
+            birdNameDisplayed.setText(birdRecord.getDataKey().getBirdName());
+            birdText.setText(birdRecord.getAbout());
+            birdImageView.getChildren().add(new ImageView(new Image(new File(birdRecord.getImage()).toURI().toString())));
+        }catch(DictionaryException ex){
+            displayAlert("ERROR: "+ ex.getMessage());
+        }
+    }
+    
+    @FXML
+    public void play(){
+        birdSound = new Media(new File(birdRecord.getSound()).toURI().toString());
+        player = new MediaPlayer(birdSound);
+        System.out.println(new File(birdRecord.getSound()).toURI().toString());
+        player.play();  
+        play.setStyle("-fx-background-color: #008000");
+        stop.setStyle("-fx-background-color: #90EE90");
+    }
+    
+    @FXML
+    public void stop(){
+        player.stop();
+        play.setStyle("-fx-background-color: #90EE90");
+        stop.setStyle("-fx-background-color: #008000");
+    }
+    
+    private int readCombo(){
+        if("Small".equals(birdSize.getValue().toString()))  return 1;
+        else if("Medium".equals(birdSize.getValue().toString()))  return 2;
+        else if("Large".equals(birdSize.getValue().toString()))  return 3;
+        else return 0;
+    }
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        birdSize.getItems().addAll("Small","Medium","Large");
+        delete.setStyle("-fx-background-color: #ff0000; ");
+        first.setStyle("-fx-background-color: #90EE90");
+        next.setStyle("-fx-background-color: #90EE90");
+        previous.setStyle("-fx-background-color: #90EE90");
+        last.setStyle("-fx-background-color: #90EE90");
+        find.setStyle("-fx-background-color: #87CEFA");
+        play.setStyle("-fx-background-color: #008000");
+        stop.setStyle("-fx-background-color: #90EE90");
         
     }
 
+    @FXML
+    public void exit() {
+        Stage stage = (Stage)mainMenu.getScene().getWindow();
+        stage.close();
+    }
+    
+    private void displayAlert(String msg) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("Alert.fxml"));
+            Parent ERROR = loader.load();
+            AlertController controller = (AlertController) loader.getController();
+
+            Scene scene = new Scene(ERROR);
+            Stage stage = new Stage();
+            stage.setScene(scene);
+
+            stage.getIcons().add(new Image("file:src/birds/WesternLogo.png"));
+            controller.setAlertText(msg);
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.showAndWait();
+
+        } catch (IOException ex) {
+
+        }
+    }
+    
+ 
 }
